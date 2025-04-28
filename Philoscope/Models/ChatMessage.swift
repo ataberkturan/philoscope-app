78530ebc21e7a1f1a3a3c9ae3454fdcc6b6ce265
Ataberk Turan <ataberk@reload.app>
2025-04-28 20:36:29 +0300

    Add HomeView, HistoryView

---
+++ Philoscope/Models/ChatMessage.swift
@@ -0,0 +1,8 @@
+//
+//  ChatMessage.swift
+//  Philoscope
+//
+//  Created by Ataberk Turan on 28/04/2025.
+//
+
+import Foundation
---
+++ Philoscope/ViewModels/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Philoscope
+//
+//  Created by Ataberk Turan on 28/04/2025.
+//
+
+import Foundation
